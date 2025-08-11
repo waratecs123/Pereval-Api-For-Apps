d4dd65ff59bd870af43593e8872a19e1f981946f
Name <netuimanetuima@gmail.com>
2025-08-11 16:23:31 +1000

    Добавлена 2 функция из Спринта №2 и доработан весь код

--- pereval/pereval_app/serializers.py
+++ pereval/pereval_app/serializers.py
@@ -9,19 +9,23 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['fam', 'name', 'otc', 'email', 'phone']
-
+        extra_kwargs = {
+            'email': {'read_only': True},
+            'last_name': {'read_only': True},
+            'first_name': {'read_only': True},
+            'patronymic': {'read_only': True},
+            'phone': {'read_only': True},
+        }
 
 class CoordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coord
         fields = ['latitude', 'longitude', 'height']
 
-
 class LevelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Level
         fields = ['winter', 'summer', 'spring', 'autumn']
-
 
 class ImageSerializer(serializers.ModelSerializer):
     data = serializers.ImageField(source="image")
@@ -30,16 +34,16 @@ class ImageSerializer(serializers.ModelSerializer):
         model = Image
         fields = ['title', 'data']
 
-
 class PerevalSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     coords = CoordSerializer()
     level = LevelSerializer()
-    images = ImageSerializer()
+    images = ImageSerializer(many=True)
 
     class Meta:
         model = Pereval
-        fields = ['beauty_title', 'title', 'other_titles', 'connect', 'add_time', 'user', 'coords', 'level', 'images']
+        fields = ['id', 'beauty_title', 'title', 'other_titles', 'connect', 'add_time', 'user', 'coords', 'level', 'images', 'status']
+        read_only_fields = ['status', 'add_time']
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
@@ -52,7 +56,7 @@ class PerevalSerializer(serializers.ModelSerializer):
             defaults={
                 'last_name': user_data['last_name'],
                 'first_name': user_data['first_name'],
-                'patronymic': user_data.ger('patronymic', ''),
+                'patronymic': user_data.get('patronymic', ''),
                 'phone': user_data['phone']
             }
         )
@@ -65,7 +69,38 @@ class PerevalSerializer(serializers.ModelSerializer):
         for image_data in images_data:
             Image.objects.create(
                 pereval=pereval,
-                title=image_data['title'],
-                image=images_data['image']
+                title=image_data.get('title', ''),
+                image=image_data['image']
             )
         return pereval
+
+    def update(self, instance, validated_data):
+        coords_data = validated_data.pop('coords', None)
+        if coords_data:
+            coord = instance.coords
+            for attr, value in coords_data.items():
+                setattr(coord, attr, value)
+            coord.save()
+
+        level_data = validated_data.pop('level', None)
+        if level_data:
+            level = instance.level
+            for attr, value in level_data.items():
+                setattr(level, attr, value)
+            level.save()
+
+        images_data = validated_data.pop('images', None)
+        if images_data:
+            instance.images.all().delete()
+            for image_data in images_data:
+                Image.objects.create(
+                    pereval=instance,
+                    title=image_data.get('title', ''),
+                    image=image_data['image']
+                )
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
--- pereval/pereval_app/urls.py
+++ pereval/pereval_app/urls.py
@@ -3,4 +3,5 @@ from .views import SubmitDataView
 
 urlpatterns = [
     path('submitData/', SubmitDataView.as_view(), name='submit-data'),
+    path('submitData/<int:pk>/', SubmitDataView.as_view(), name='submit-data-detail'),
 ]
